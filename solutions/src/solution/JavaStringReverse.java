e596b38b5ffd4b2c1a7df285330366926c14dd24
tothetop430 <jhuseyrer355545@gmail.com>
2022-03-28 23:39:42 +0530

    solve JavaStringReverse

---
+++ solutions/src/solution/JavaStringReverse.java
@@ -0,0 +1,15 @@
+// https://www.hackerrank.com/challenges/java-string-reverse/problem?isFullScreen=true
+
+package solution;
+
+import java.util.Scanner;
+
+public class JavaStringReverse {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        System.out.println(A.equals(new StringBuffer(A).reverse().toString()) ? "Yes" : "No");
+    }
+
+}
