3530aaa68a35b67aa8c21359030e2e1b17bc5b9a
tothetop430 <jhuseyrer355545@gmail.com>
2022-03-30 21:57:08 +0530

    solve JavaArrayPart2

---
+++ solutions/src/solution/JavaArrayPart2.java
@@ -0,0 +1,33 @@
+// https://www.hackerrank.com/challenges/java-1d-array/problem
+
+package solution;
+
+import java.util.Scanner;
+
+public class JavaArrayPart2 {
+
+    public static boolean canWin(int leap, int[] game, int i) {
+        if (i<0 || game[i] == 1) return false;
+        if (i == game.length-1 || i+leap >= game.length) return true;
+        game[i] = 1;
+        return canWin(leap, game, i+1) || canWin(leap, game, i-1) || canWin(leap, game, i+leap);
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int q = scan.nextInt();
+        while (q-- > 0) {
+            int n = scan.nextInt();
+            int leap = scan.nextInt();
+
+            int[] game = new int[n];
+            for (int i = 0; i < n; i++) {
+                game[i] = scan.nextInt();
+            }
+
+            System.out.println( (canWin(leap, game, 0)) ? "YES" : "NO" );
+        }
+        scan.close();
+    }
+
+}
