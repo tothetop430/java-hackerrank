8d00ec3e90622e1b311ae98daf6e4c6f9346ef2f
tothetop430 <jhuseyrer355545@gmail.com>
2022-04-01 21:56:00 +0530

    solve JavaInheritance1

---
+++ solutions/src/solution/oops/JavaInheritance1.java
@@ -0,0 +1,28 @@
+// https://www.hackerrank.com/challenges/java-inheritance-1/problem
+
+package solution.oops;
+
+public class JavaInheritance1 {
+    public static void main(String args[]){
+        Bird bird = new Bird();
+        bird.walk();
+        bird.fly();
+        bird.sing();
+    }
+}
+
+class Animal{
+    void walk(){
+        System.out.println("I am walking");
+    }
+}
+
+class Bird extends Animal{
+    void fly(){
+        System.out.println("I am flying");
+    }
+
+    void sing() {
+        System.out.println("I am singing");
+    }
+}
