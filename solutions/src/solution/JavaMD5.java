9fff6491883a7fba03d1e35a4c293f67f8fae7fc
tothetop430 <jhuseyrer355545@gmail.com>
2022-04-02 17:47:36 +0530

    solve JavaMD5

---
+++ solutions/src/solution/JavaMD5.java
@@ -0,0 +1,24 @@
+// https://www.hackerrank.com/challenges/java-md5/problem
+
+package solution;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
+public class JavaMD5 {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        scanner.close();
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] byteArray = md.digest(input.getBytes());
+        BigInteger no = new BigInteger(1, byteArray);
+        String hashtext = no.toString(16);
+        while (hashtext.length() < 32) {
+            hashtext = "0" + hashtext;
+        }
+        System.out.println(hashtext);
+    }
+}
